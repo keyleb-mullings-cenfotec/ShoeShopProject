9141527a6b533ce317b8069d51baaaa9353421f3
keyleb-mullings-cenfotec <60416684+keyleb-mullings-cenfotec@users.noreply.github.com>
2022-05-10 22:23:07 -0600

    Testing searchbar

--- screens/ItemsScreen.tsx
+++ screens/ItemsScreen.tsx
@@ -1,14 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Input, SearchBar } from 'react-native-elements'
 
 const ItemsScreen = () => {
   return (
-    <View>
-      <Text>ItemsScreen</Text>
+    // <Text>ItemsScreen</Text>
+    <View style={styles.itemsPage}>
+      <View style={styles.searchBarContainer}>
+        <SearchBar
+          placeholder="Search"
+          lightTheme={true}
+          containerStyle={{backgroundColor:'#fff'}}
+          // onChangeText={onChangeSearch}
+          // value={searchQuery}
+        />
+      </View>
     </View>
   )
 }
 
 export default ItemsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  itemsPage: {
+    flex: 1,
+    backgroundColor:'#fff'
+  },
+  searchBarContainer: {
+    maxWidth: '85%',
+    paddingLeft: 3,
+    // borderWidth: 2,
+    marginLeft: '7%',
+    marginTop: '2%',
+    // borderColor: '#B8B5B5',
+    // borderRadius: 5
+
+    // justifyContent: 'center',
+    // alignItems: 'center'
+  },
+})
